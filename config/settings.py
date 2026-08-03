53e2b5352040300e64b8661185eecd31ea935ac7
Oyugijames1993 <oyugiochieng1@gmail.com>
2026-08-03 09:39:59 +0300

    feat: permanent download app banner at top of chat

--- config/settings.py
+++ config/settings.py
@@ -240,16 +240,18 @@ CHANNEL_LAYERS = {
 }
 
 # ── AWS S3 File Storage ───────────────────────────────────────────────────────
-AWS_ACCESS_KEY_ID       = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY   = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'tutorjamesconnect-media')
-AWS_S3_REGION_NAME      = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
-AWS_S3_CUSTOM_DOMAIN    = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-AWS_DEFAULT_ACL         = 'public-read'
+AWS_ACCESS_KEY_ID        = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY    = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME  = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'tutorjamesconnect-media')
+AWS_S3_REGION_NAME       = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
+AWS_S3_CUSTOM_DOMAIN     = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+AWS_DEFAULT_ACL          = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_QUERYSTRING_AUTH    = False
+AWS_QUERYSTRING_AUTH     = False
+AWS_S3_FILE_OVERWRITE    = False
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-# Use S3 in production, local storage in development
 if AWS_ACCESS_KEY_ID:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
