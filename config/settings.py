30e252f38c1b25e4516ba9bdfbd9c784dbbc6152
Oyugijames1993 <oyugiochieng1@gmail.com>
2026-08-03 09:51:03 +0300

    fix: force public-read ACL on S3 file uploads

--- config/settings.py
+++ config/settings.py
@@ -246,12 +246,16 @@ AWS_STORAGE_BUCKET_NAME  = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'tutorjames
 AWS_S3_REGION_NAME       = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
 AWS_S3_CUSTOM_DOMAIN     = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 AWS_DEFAULT_ACL          = 'public-read'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read',
+}
 AWS_QUERYSTRING_AUTH     = False
 AWS_S3_FILE_OVERWRITE    = False
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
+
 if AWS_ACCESS_KEY_ID:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'config.storage_backends.PublicMediaStorage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
