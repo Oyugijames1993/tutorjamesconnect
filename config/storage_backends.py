c38355c0442d3b62f1abd4496a6929d53d76ebd6
Oyugijames1993 <oyugiochieng1@gmail.com>
2026-08-04 11:23:10 +0300

    fix: set correct S3 custom domain with region in storage backend

--- config/storage_backends.py
+++ config/storage_backends.py
@@ -1,6 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+import os
 
 class PublicMediaStorage(S3Boto3Storage):
     default_acl    = 'public-read'
     file_overwrite = False
-    custom_domain  = False
+    region_name    = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
+    custom_domain  = f"{os.environ.get('AWS_STORAGE_BUCKET_NAME', 'tutorjamesconnect-media')}.s3.{os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')}.amazonaws.com"
+    querystring_auth = False
